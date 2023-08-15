6d75522206f068fd26a3d635f55d1a9991fd4479
jun <ndh001@github.com>
2023-08-15 21:49:01 +0800

    last check

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         "USER": os.environ.get("DBUSER"),
         "PASSWORD": os.environ.get("DBPASS"),
         "PORT": "5432",
-        "HOST": "54.179.176.85",
+        "HOST": "127.0.0.0",  # "54.179.176.85",
         "OPTIONS": {
             "options": "-c search_path=django,traveldata,userdata,journingdata,collectiondata"
         },

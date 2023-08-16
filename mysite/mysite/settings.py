39f43828bf59167dad3e7446d3691d9c6fae15b1
jun <ndh001@github.com>
2023-08-16 12:45:14 +0800

    last check

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         "USER": os.environ.get("DBUSER"),
         "PASSWORD": os.environ.get("DBPASS"),
         "PORT": "5432",
-        "HOST": "127.0.0.1",  # "db-jun-instance.c8dr5e4c6fd8.ap-southeast-1.rds.amazonaws.com",
+        "HOST": "localhost",  # "db-jun-instance.c8dr5e4c6fd8.ap-southeast-1.rds.amazonaws.com",
         "OPTIONS": {
             "options": "-c search_path=django,traveldata,userdata,journingdata,collectiondata"
         },

2fd36c308cb2deda82d4102c431a3aaa139e1876
jun <ndh001@github.com>
2023-08-17 20:58:14 +0800

    last check

---
+++ migrates.sql
@@ -0,0 +1,6 @@
+\copy traveldata.cities from '~/travelWeb_scrapper/csv/cities_n_links.csv' delimiter ',' header csv;
+\copy traveldata.foods from '~/travelWeb_scrapper/csv/food_all.csv' delimiter ',' header csv;
+\copy traveldata.shops from '~/travelWeb_scrapper/csv/shopping_all.csv' delimiter ',' header csv;
+\copy traveldata.sights from '~/travelWeb_scrapper/csv/sight_all.csv' delimiter ',' header csv;
+\copy traveldata.sights_texts from '~/travelWeb_scrapper/csv/sight_text_all.csv' delimiter ',' header csv;
+\copy traveldata.sights_imgs from '~/travelWeb_scrapper/csv/sight_imgs_all.csv' delimiter ',' header csv;

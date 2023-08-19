b37ba708b969640660abcec8f52dc26bf5165f9a
jun <ndh001@github.com>
2023-08-19 14:21:53 +0800

    last check

--- mysite/journing/static/journing/scripts/index.js
+++ mysite/journing/static/journing/scripts/index.js
@@ -11,3 +11,30 @@ function smooth_scroll(){
 }
 
 smooth_scroll()
+let first_page = document.querySelector('.index-main-first-page')
+let page_nav= document.querySelector('.page_nav')
+
+function init_page_nav(){
+
+    if (first_page && window.innerWidth >= 1500){
+        page_nav.style.display='none'
+        window.addEventListener('scroll',()=>{
+
+        if (window.scrollY >= 800){
+            page_nav.style.display = 'block'
+        }
+        else{
+            page_nav.style.display='none'
+        }
+        })
+
+    }else{
+        page_nav.style.display='block'
+    } 
+}
+
+init_page_nav()
+
+function show_page_nav(){
+
+}

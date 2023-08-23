a4373d415214157b7cca541eab0ffc942f34f68a
jun <ndh001@github.com>
2023-08-23 17:44:26 +0800

    last check

--- mysite/journing/static/journing/scripts/new_journal.js
+++ mysite/journing/static/journing/scripts/new_journal.js
@@ -1,16 +1,26 @@
-let collection = undefined
+let activity_name = undefined
 let list_name = undefined
 function get_data(data,list){
-    collection=data
+    activity_name=data
     list_name=list
 }
 function restore_div(index){
     div = $(`#hour-div-${index}`)
+    ori_list = div.find('.activity').find('img').attr('id')
+    ori_list = '.' + ori_list + '-list';
+    
+    current_element = div.find('.drop-area').find('div')
+    current_element.css({
+        'height':'75px',
+        'width':'120px'
+    })
+
+    
+    $(ori_list).append(current_element)
+
     div.find('.drop-area').empty()
     div.find('.name').text('-')
-    original.css({'height':'80px','width':'120px'})
-    $('.sight-list').append(original)
-    original=null
+    div.find('.activity').html('')
 
 }
 
@@ -23,27 +33,49 @@ $(document).ready(function(){
         appendTo:'body',
         helper:'clone', 
         start:function(event,ui){
-            ui.helper.data({'data':collection})
+            ui.helper.data({'activity_name':activity_name})
             ui.helper.data({'list_name':list_name})
-            original = $(this)
-
         }
         
-    })
-    
+    }) 
     drop.droppable({
         accept: ".collection-img",
         drop: function(event, ui) {
-            if ($(this).children().length===0){
-                $(this).append(ui.draggable);
-                ui.draggable.css({'height':'100%','width':'100%','border-radius':'10px'}); // Reset the image position
-                ui.draggable.find('img').css({'height':'100%','width':'100%','border-radius':'5px'})
-                time = $(this).attr('id')
-                $(`#hour-div-${time} .name`).text(ui.helper.data('data'))
-                $(`#hour-div-${time} .activity`).html(
-                    `<img src="${sighticon}"/>`
-                )
+            time = $(this).attr('id')
+            current_element = $(this)
+            
+            if ($(this).children().length>0){
+                restore_div(time)
+
             }
+                $(this).append(ui.draggable);
+
+                ui.draggable.css({'height':'100%','width':'100%','border-radius':'10px'}); 
+                ui.draggable.find('img').css({'height':'100%','width':'100%','border-radius':'5px'})
+
+                $(`#hour-div-${time} .name`).text(ui.helper.data('activity_name'))
+                
+                let selected_icon = undefined
+                let chosen_id = undefined
+                
+                
+                console.log(ui.helper.data('list_name'))
+                console.log(ui.helper.data('activity_name'))
+                
+                if (ui.helper.data('list_name') === 'sight_collections'){
+                    selected_icon = sighticon
+                    chosen_id = 'sight'
+                }else if (ui.helper.data('list_name')==='food_collections'){
+                    selected_icon = foodicon 
+                    chosen_id = 'food'
+                }else{
+                    selected_icon = shopicon
+                    chosen_id = 'shop'
+                }
+
+                $(`#hour-div-${time} .activity`).html(
+                    `<img id="${chosen_id}" src="${selected_icon}"/>`
+                )
     }
     })
 
